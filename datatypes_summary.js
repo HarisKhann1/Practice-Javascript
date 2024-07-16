309579fe2e106447d0795edf37f7539132c0f23f
Haris Khan <hariskhan3428@gamil.com>
2024-07-16 13:21:35 +0500

    Summary of Datatypes in js

---
+++ datatypes_summary.js
@@ -0,0 +1,20 @@
+/*
+
+Primitive Datatypes (7 types):
+It is called call by value, cannot provide the original refrence of the data, the changes we do, is in the copy not in the original data.
+
+1) String
+2) Number
+3) Boolean
+4) null
+5) undefined
+6) Symbol
+7) BigInt
+
+Refrence (Non-primitive)
+It is call by refrence, opposite of Primitive.
+
+1) Array
+2) Objects
+3) Functions
+*/
