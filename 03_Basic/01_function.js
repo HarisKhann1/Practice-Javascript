f71d760fbc3ea98b9882e39c3a71538efbb27040
Haris Khan <hariskhan3428@gamil.com>
2024-07-26 17:00:03 +0500

    function in js

---
+++ 03_Basic/01_function.js
@@ -0,0 +1,23 @@
+//  all about function in js
+
+// function functionName(params, params){
+//     // logic
+// }
+
+// function addTwoNumbers(num1, num2) {
+//     console.log(num1 * num2);
+//     // return num1 * num2
+// }
+
+// let result = addTwoNumbers(3,4)
+// console.log(`Result: ${result}`);
+
+
+// rest operator
+function calculateCardPrice(...num1) {
+    return num1
+}
+
+console.log(calculateCardPrice(23,12,31,12));
+
+
