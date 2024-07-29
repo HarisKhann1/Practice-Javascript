ae804917f05d815e62d6b8fdc0b24ea12f9e91e4
Haris Khan <hariskhan3428@gmail.com>
2024-07-29 14:58:46 +0500

    while loop in js

---
+++ 05_Loops/whileLoop.js
@@ -0,0 +1,17 @@
+let i = 0
+// while (i < 10) {
+//     console.log(i);
+//     i += 1
+// }
+
+const arr = [1,23,4,3,5]
+
+while (i < arr.length) {
+    console.log(arr[i]);
+    i = i + 1
+}
+
+do {
+    console.log(i);
+    i++
+} while (i < 10);
