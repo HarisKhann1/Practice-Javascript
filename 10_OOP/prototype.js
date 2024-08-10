957f06da7c802b140b3cb571ef78c204d755414e
unknown <hariskhan3428@gmail.com>
2024-08-10 23:17:10 +0500

    prototype in js

---
+++ 10_OOP/prototype.js
@@ -0,0 +1,28 @@
+const superHeros = ['spiderman', 'hulk']
+
+const superHerosPower = {
+    spiderman: "temp",
+    hulk: "temp2",
+
+    power: function() {
+        console.log(`power of hulk ${this.hulk}`);
+    }
+}
+
+// adding method at object level
+
+Object.prototype.hello = function() {
+    console.log("Hello, access me");
+}
+
+// now the hello function will be array and object as well
+superHeros.hello()
+superHerosPower.hello()
+
+// adding another method to array object only
+Array.prototype.print = function(){
+    console.log(`This is print function`);
+}
+
+superHeros.print()
+superHerosPower.print()
